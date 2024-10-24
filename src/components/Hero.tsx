bf64e05a7de0d312145ffc2bcc3716241c857a06
amber officil <amberofficial380@gmail.com>
2024-10-24 09:13:06 +0500

    milestone2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
           <div className="hidden lg:block"></div>
           <div className="text-[80px] sm:text-[90px] font-bold leading-tight flex justify-center items-center">
-            <div>
+            <div className='mt-[100px]'>
               <p data-aos="zoom-in-up">I</p>
               <p data-aos="zoom-in-up">am</p>
               <p data-aos="zoom-in-up">Amber</p>
--- src/components/Navebar.tsx
+++ src/components/Navebar.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import { AiOutlineMenu } from "react-icons/ai";
-const Navebar = () => {
+"use client"
+import React, { useState } from 'react'
+
+const Navebar = () =>{
+const [isClick, setisClick] = useState(false);
+const toggleNavebar = () : void => {
+  setisClick(!isClick)
+}
   return (
+    <>
     <div className="container pt-8">
         <div className="flex justify-between items-center">
             <div className="text-xl font-medium">Amber</div>
@@ -12,10 +18,67 @@ const Navebar = () => {
                 <li className="menuLink"><a href="#skills">Skills</a></li>
                 <li className="menuLink"><a href="#contact">Contact</a></li>
             </ul>
-            <AiOutlineMenu className="md:hidden" size={30} />
+            <div className='md:hidden flex items-center'>
+              <button
+              className="inline-flex items-center justify-center p-2 rounded-md text-whit hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white "
+              onClick={toggleNavebar} 
+              >
+                {isClick ? (
+                  <svg
+                  className='h-6 w-6'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  >
+                    <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M6 18L18 6M6 6l12 12'
+                    />
+
+                  </svg>
+                ) : (
+                  <svg
+                  className='h-6 w-6'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  >
+                    <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth={2}
+                    d='M4 6h16M4 12h16m-7 6h7'
+                    />
+
+                  </svg>
+                )}
+
+              </button>
+            
         </div>
-      
-    </div>
+   </div>
+</div>
+{isClick && (
+  <div className='md:hidden'>
+   <div>
+    <div  className='px-2 pt-1 space-y-1 sm:px-3'>
+   <ul className='flex justify-between items-center'>
+                <li className="menuLink"><a href="#hero">Home</a></li>
+                <li className="menuLink"><a href="#about">About</a></li>
+                <li className="menuLink"><a href="#projects">Projects</a></li>
+                <li className="menuLink"><a href="#skills">Skills</a></li>
+                <li className="menuLink"><a href="#contact">Contact</a></li>
+            </ul>
+            </div>
+   </div>
+  </div>
+)}
+        
+    </>
   )
 }
 
